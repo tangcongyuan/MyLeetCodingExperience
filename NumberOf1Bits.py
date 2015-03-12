ca1aa57f6ec9ea138a46add3817a7e936bce3894
Eric (Congyuan) Tang <tangcongyuan@users.noreply.github.com>
2015-03-12 18:15:24 -0400

    Update NumberOf1Bits.py

--- NumberOf1Bits.py
+++ NumberOf1Bits.py
@@ -2,7 +2,7 @@ class Solution:
     # @param n, an integer
     # @return an integer
     def hammingWeight(self, n):
-        s = (bin(n)[2:])
+        s = (bin(n)[2:]) # convert integer n to binary number and slice the '0b' out of it.
         count = 0
         for number in s:
             if number == '1':
